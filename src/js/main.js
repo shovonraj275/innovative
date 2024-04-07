d24a86b13c1baba28f89c6e6cc1b22073d78081d
Ezaz ahamed <73046376+shovonraj275@users.noreply.github.com>
2024-04-07 22:55:41 +0600

    bottom header and slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,18 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 
-// function opneNav(){
-//   document.getElementById("mobileNav").style.width ="100%"
-// }
-
-// function closeNav(){
-//   document.getElementById("mobileNav").style.width ="0%"
-// }
 
 $(document).ready(function(){
   $('.slider').bxSlider({
     auto:true,
     
-  });
-});
-
-$(".progress-bar").loading();
-
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-      items:4,
-      loop:true,
-      autoplay:true,
   });
 });
