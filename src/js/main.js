e1501a058da31ec9a98963d4f94dce9f5dfeb140
shvovon <73046376+shvovon@users.noreply.github.com>
2024-04-09 12:42:16 +0600

    xjjjj

--- src/js/main.js
+++ src/js/main.js
@@ -39,8 +39,11 @@ $(document).ready(function(){
 });
 
 
-  /* construct manually */
-  var bar1 = new ldBar("#myItem1");
-  /* ldBar stored in the element */
-  var bar2 = document.getElementById('myItem1').ldBar;
-  bar1.set(60);
+
+
+
+  // /* construct manually */
+  // var bar1 = new ldBar("#myItem1");
+  // /* ldBar stored in the element */
+  // var bar2 = document.getElementById('myItem1').ldBar;
+  // bar1.set(60);
