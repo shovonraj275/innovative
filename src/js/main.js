4cfb628d496e9c48fe70c61a61fba34338f4e053
Ezaz ahamed <73046376+shovonraj275@users.noreply.github.com>
2024-04-10 22:54:46 +0600

    updwted

--- src/js/main.js
+++ src/js/main.js
@@ -8,27 +8,49 @@ function myFunction() {
   }
 }
 
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:false,
-  items:4,
-  autoplay:true,
-  responsiveClass: true,
-  responsive: {
-      0:{
-        items: 1
-      },
-      480:{
-        items: 2
-      },
-      769:{
-        items: 4
-      }
-  }
+// $('.owl-carousel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   nav:false,
+//   items:4,
+//   autoplay:true,
+//   responsiveClass: true,
+//   responsive: {
+//       0:{
+//         items: 1
+//       },
+//       480:{
+//         items: 2
+//       },
+//       769:{
+//         items: 4
+//       }
+//   }
+// })
+$(document).ready(function () {
+
+  $(".owl-carousel").owlCarousel({
+      // autoPlay: true,
+      margin: 10,
+      items: 4,
+      responsive:{
+              525:{
+                  items:1,
+              },
+              787:{
+                  items:2,
+              },
+              992:{
+                  items:4,
+              },
+}
+  });
+
 })
 
-
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
 
 $(document).ready(function(){
   $('.slider').bxSlider({
